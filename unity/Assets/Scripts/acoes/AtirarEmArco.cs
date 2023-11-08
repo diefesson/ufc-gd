0e132824512e9db57a3cfd7a40747c832107a188
diefesson <diefesson.so@gmail.com>
2023-11-08 10:13:21 -0300

    Componentes fornecidos pelo o professor

---
+++ unity/Assets/Scripts/acoes/AtirarEmArco.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtirarEmArco : MonoBehaviour
+{
+    public GameObject projetil;
+    public int numeroDirecoes;
+    public float tempoEntreDisparos;
+    public float tempoMinimoAntesComecarAtirar;
+    public float tempoMaximoAntesComecarAtirar;
+    private int angulo;
+    private Quaternion rotacaoInicial;
+
+    void Start()
+    {
+        angulo = 180 / numeroDirecoes;
+        rotacaoInicial = transform.localRotation;
+        
+        StartCoroutine(AtiraContinuamente());
+    }
+    
+    IEnumerator AtiraContinuamente()
+    {
+        yield return new WaitForSeconds(
+            Random.Range(tempoMinimoAntesComecarAtirar, tempoMaximoAntesComecarAtirar));
+
+        while (true)
+        {
+            Atira();
+
+            yield return new WaitForSeconds(tempoEntreDisparos);
+        }
+    }
+
+    public void Atira()
+    {
+        // 90, pois é a metade de 180 graus
+        transform.Rotate(0.0f, 0.0f, -(90 - (angulo / 2)), Space.Self);
+        Instantiate(projetil, transform.position, transform.rotation);
+
+        // for (int i = 2; i <= numeroDirecoes; i++)
+        // OU
+        for (int i = 1; i <= numeroDirecoes - 1; i++)
+        {
+            transform.Rotate(0.0f, 0.0f, angulo, Space.Self);
+            Instantiate(projetil, transform.position, transform.rotation);
+        }
+
+        transform.localRotation = rotacaoInicial;
+    }
+
+    public void SetProjetil(GameObject proj)
+    {
+        projetil = proj;
+    }
+
+    public void SetNumeroDirecoes(int numDir)
+    {
+        numeroDirecoes = numDir;
+    }
+
+    public void VariacaoNumeroDirecoes(int variacao)
+    {
+        numeroDirecoes += variacao;
+
+        if (numeroDirecoes <= 0)
+            numeroDirecoes = 1;
+    }
+}
---
+++ unity/Assets/Scripts/acoes/AtirarEmArcoConcentrado.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtirarEmArcoConcentrado : MonoBehaviour
+{
+    public GameObject projetil;
+    public int numeroDirecoes;
+    public float tempoEntreDisparos;
+    public float tempoMinimoAntesComecarAtirar;
+    public float tempoMaximoAntesComecarAtirar;
+    public float anguloEntreDirecoes;
+    private float angulo;
+    private float anguloTotal;
+    private Quaternion rotacaoInicial;
+
+    void Start()
+    {
+        anguloTotal = anguloEntreDirecoes * (numeroDirecoes - 1);
+        angulo = anguloTotal / numeroDirecoes;
+        rotacaoInicial = transform.localRotation;
+
+        StartCoroutine(AtiraContinuamente());
+    }
+
+    IEnumerator AtiraContinuamente()
+    {
+        yield return new WaitForSeconds(
+            Random.Range(tempoMinimoAntesComecarAtirar, tempoMaximoAntesComecarAtirar));
+
+        while (true)
+        {
+            Atira();
+
+            yield return new WaitForSeconds(tempoEntreDisparos);
+        }
+    }
+
+    public void Atira()
+    {
+        transform.Rotate(0.0f, 0.0f, -((anguloTotal / 2) - (angulo / 2)), Space.Self);
+        Instantiate(projetil, transform.position, transform.rotation);
+
+        for (int i = 1; i <= numeroDirecoes - 1; i++)
+        {
+            transform.Rotate(0.0f, 0.0f, angulo, Space.Self);
+            Instantiate(projetil, transform.position, transform.rotation);
+        }
+
+        transform.localRotation = rotacaoInicial;
+    }
+
+    public void SetProjetil(GameObject proj)
+    {
+        projetil = proj;
+    }
+
+    public void SetNumeroDirecoes(int numDir)
+    {
+        numeroDirecoes = numDir;
+    }
+
+    public void VariacaoNumeroDirecoes(int variacao)
+    {
+        numeroDirecoes += variacao;
+
+        if (numeroDirecoes <= 0)
+            numeroDirecoes = 1;
+    }
+
+    public void SetAnguloEntreDirecoes(int angEntreDir)
+    {
+        anguloEntreDirecoes = angEntreDir;
+    }
+
+    public void VariacaoAnguloEntreDirecoes(int variacao)
+    {
+        anguloEntreDirecoes += variacao;
+
+        if (anguloEntreDirecoes <= 0)
+            anguloEntreDirecoes = 1;
+    }
+}
